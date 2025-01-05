9b73d08412031dcf40ff17a5b6df71ebdad15915
gdr <dirocco.gli@gmail.com>
2025-01-05 18:50:24 +0100

    Inserimento sistema clips, rewindTo, fastForward

--- run.py
+++ run.py
@@ -8,6 +8,11 @@ from trajectory import Trajectory
 from camera_manager import Camera
 from database import *
 
+#Inizializzazioni
+#gestore fisica
+#physiscsManager = PhysicsManager(bodies)
+
+
 #seed fighi
 #4487033921494198826
 #3686757135635109634
@@ -19,10 +24,16 @@ class Main():
 		self.win.tracer(False)
 		self.win.title("Gravity Simulator X")
 		self.win.setup(1200, 800)
+		self.clip_log = []
+
+		self.user_input = ""
 
 		turtle.speed(speed="fastest")
 
 		self.lastSeed = random.randrange(sys.maxsize)
+
+
+		connect_to_database()
 	
 	def showcase_system(self):
 		self.bodies = [Planet(1, -600, 0, 0, -2000), 
@@ -71,6 +82,7 @@ class Main():
 		self.rng = random.Random(seed)
 		print(seed)
 
+		#aggiungere bottoni per rewind, restartWithLastSeed, forward e fastForward
 		self.win.clear()
 		self.win.bgcolor("black")
 		self.win.tracer(0)
@@ -79,6 +91,14 @@ class Main():
 		self.win.onkeypress(self.timeStep.rewind, "b")
 		self.win.onkeypress(self.timeStep.forward, "f")
 		self.win.onkeypress(self.timeStep.fastForward, "h")
+		#l'utente inseriesce le cifre (caratteri numerici) con i tasti da 0 a 9
+		for c in "0123456789":
+			self.win.onkey(lambda d=c: self.inserimento(d), c)
+		self.win.onkey(lambda: self.inserimento("BackSpace"), "BackSpace")
+		self.win.onkey(lambda: self.inserimento("Return"), "Return")
+		self.win.onkey(lambda: exit(), "q")  
+		self.win.listen()
+		self.win.onscreenclick(onClick)
 		self.win.listen()
 
 		self.showcase_system()
@@ -92,7 +112,7 @@ class Main():
 		if try_id:
 			for body in self.bodies:
 				if isinstance(body, Planet):  #solo i pianite vengono registrati
-					insert_planet(try_id, body)
+					self.insert_planet(try_id, body)
 
 		self.camera = Camera()
 
@@ -128,10 +148,171 @@ class Main():
 				body.updateAll(self.timeStep.getAbsStepTime())
 	
 		self.win.ontimer(self.mainLoop, 10)
+	
+	#ok
+	def salva_clip(self):
+		self.centratura = 1.0
+		clip = self.timeStep.creaClip(self.bodies, self.centratura)
+		self.clip_log.append(clip)
+		print(f"Clip salvata: {clip}")
+	
+	#new
+		clip_id = self.insert_clip(clip['time_step'], clip['planet_count'], clip['planet_ids'], clip['planet_coords'])
+		if clip_id:
+			print(f"Clip salvata nel db con ID: {clip_id}")
+
+	#ok
+	def insert_clip(self, time_step, planet_count, planet_ids, planet_coords):
+		if db_connection is None:
+			print("Connessione al db non disponibile.")
+			return None
+		try:
+			cursor = db_connection.cursor()
+			query = """ INSERT INTO clips (time_step, planet_count, planet_ids, planet_coords)
+			VALUES (%s, %s, %s, %s)"""
+
+			cursor.execute(query(time_step, planet_count, str(planet_ids), str(planet_coords)))
+			db_connection.commit()
+
+			cursor.execute("SELECT LAST_INSERT_ID();")
+			clip_id = cursor.fetchone()[0]
+			return clip_id
+		except Error as e:
+			print(f"Errore dirante l'inserimentod ella clip: {e}")
+			return None
+
+	#ok
+	#rewindToTime
+	def rewindToT(self):
+		#leggo l'input dell'utente
+		if self.user_input.isdigit():
+			time_step = int(self.user_input)
+			clip = self.timeStep.rewindTo(time_step)
+
+			if clip:
+				print(f"Il sistema è tornato a {clip['time_step']} con stabilità {clip['stabilita']}.")
+				self.reset_sistema()
+			else:
+				print("Nessuna clip disponibile per il tempo specificato")	
+		else:
+			print("Valore inserito non valido, riprovare.")
+
+	#ok
+	def reset_sistema(self):
+		for body in self.bodies:
+			body.clear()
+		self.timeStep.reset()	
+
+
+	def insert_planet(self, clip_id, planet):
+		query = """
+    	INSERT INTO planets (clip_id, name, mass, x_coord, y_coord) 
+    	VALUES (%s, %s, %s, %s, %s);
+    	"""
+		cursor.execute(query, (clip_id, planet.name, planet.mass, planet.x, planet.y))
+		connection.commit()
+
+	def connect_to_database(self):
+		"""Metodo per connettersi al database (supponendo l'uso di MySQL)"""
+		global connection, cursor
+		connection = pymysql.connect(host='localhost', user='root', password='password', database='gravity_simulator')
+		cursor = connection.cursor()
+
+	def close_database(self):
+		"""Chiude la connessione al database"""
+		cursor.close()
+		connection.close()
+
+	def inserimento(self, key):
+		#cancella
+		if key == "BackSpace":
+			self.user_input = self.user_input[:-1]
+		#invio -> non succede nulla
+		elif key == "Return":
+			return
+		#inserimento cifra
+		elif key.isdigit():
+			self.user_input += key
+		self.upd_input(casella_input, self.user_input)
+
+	def upd_input(self, input_box, text):
+		input_box.clear() #resetto il contenuto della casella
+		input_box.write(
+			text,
+			align="left",
+			font=("Arial", 14, "normal")
+		)
+
+
+def onClick(mouse_x, mouse_y):
+		for button in buttons:
+			if button["x"] <= mouse_x <= button["x"] + button["w"] and button["y"] <= mouse_y <= button["y"] + button["h"]:
+				print(mouse_x)
+				button["callback"]()
+
+
+buttons = []
+
+#Per creare un bottone cliccabile
+#argomenti: x,y -> posizione del bottone; w, h -> dimensioni; text -> testo; callback -> funzione eseguita al click del bottone
+def create_button(x, y, w, h, text, callback):
+	button = turtle.Turtle()
+	button.hideturtle()
+	button.penup()
+	button.goto(x, y)
+	button.pendown()
+	button.color("white", "gray")
+	button.begin_fill()
+	for i in range(2):
+		button.forward(w)
+		button.left(90)
+		button.forward(h)
+		button.left(90)
+	button.end_fill()
+
+	#testo bottone
+	button.penup()
+	button.goto(x + w / 2, y + h / 2 - 10)  
+	button.color("white")
+	button.write(text, align="center", font=("Arial", 14, "bold"))
+	buttons.append({"x": x, "y": y, "w": w, "h": h, "callback": callback})
+#Per creare un bottone cliccabile
+#argomenti: x,y -> posizione del bottone; w, h -> dimensioni; text -> testo; callback -> funzione eseguita al click del bottone
+
+#casella x input utente
+def create_input_box(x, y, w, h):
+    input_box = turtle.Turtle()
+    input_box.hideturtle()
+    input_box.penup()
+    input_box.goto(x, y)
+    input_box.pendown()
+    input_box.color("white", "black")
+    input_box.begin_fill()
+    input_box.forward(w) #larghezza
+    input_box.left(90)
+    input_box.forward(h) #altezza
+    input_box.left(90)
+    input_box.forward(w)
+    input_box.left(90)
+    input_box.forward(h)
+    input_box.left(90)
+    input_box.end_fill()
+
+    input_box.penup()
+    input_box.goto(x + 5, y + h - 25) #posizione testo nel bottone
+    return input_box
+
 
 if __name__ == "__main__":
 	app = Main()
-	connect_to_database()  # Connessione al database
 	app.restart()
+	#app.mainLoop()
+	#Bottone salvaClip
+	create_button(-550, 300, 100, 40, "Salva clip", app.salva_clip)
+	#Bottone rewindTo
+	create_button(-550, 350, 100, 40, "Torna a", app.rewindToT)
+	#casella x input
+	casella_input = create_input_box(-430, 350, 100, 40)
 	app.mainLoop()
+	connect_to_database()  # Connessione al database
 	app.win.mainloop()
--- time_manager.py
+++ time_manager.py
@@ -1,13 +1,22 @@
 import math
 
+#---
+#metodi mancanti
+#
+
 class TimeStep:
+	MAX_STATE_LOG = 1000 #non loggare piu di 1000 clip
 	def __init__(self, step: float = 0.001, tempo: int = 1):
 		self.step = step
 		self.elapsed_time = 0.0
 		self.base_tempo = tempo
 		self.tempo = self.base_tempo
+
 		self.timer_flags = {}
-	
+		self.clips = {}  #dizionario per memorizzare le clip (snapshot); chiave: time, valore: dati dello screenshot
+		self.state_log = []  #log stati per fastF e rewind
+		self.current_state = None
+	#---GESTIONE TEMPO
 	def nextStep(self):
 		self.elapsed_time += self.step
 	
@@ -52,3 +61,76 @@ class TimeStep:
 	def setTempo(self, tempo_string="1x"):
 		self.tempo = self.base_tempo = int(tempo_string[:tempo_string.index("x")])
 		print("tempo:", self.tempo)
+
+	
+	#---CLIPS: 'momenti' catturati in modo deciso dall'utente
+
+	#ogni clip include: numerpo di corpi, ID corpi, coeff. stabilita 
+	def creaClip(self, bodies, centratura):
+		stabilita = self.getElapsedTime() * centratura
+		clip = {
+			"time_step": self.getElapsedTimeInt(),
+			"planet_count": len(bodies),
+			"planet_ids" : [id(body) for body in bodies],
+			"planet_coords" : None,
+			"stabilita": stabilita,
+		}
+
+		#salvo la clip nel dizionario (self.clips)
+		#chiave: self.getElapsedTimeInt()
+		#valore: clip
+		self.clips[self.getElapsedTimeInt()] = clip
+		return clip
+	
+	#richiamare una clip dal dizionario self.clips
+	#chiave (tempo): time_step
+	#nessuna clip per il tempo 'time_step' (int)-> ritorna None
+	def getClip(self, time_step):
+		return self.clips.get(time_step, None) #get: metodo per cercare un valore in dizionario usando una chiave
+	
+
+	#---SALTI
+
+
+	#usa getClip per riportare il sistema al momento 'time_step'
+	#funziona solo se l'utente ha creato una clip per il time_step
+	def rewindTo(self, time_step):
+		clip = self.getClip(time_step)
+		if clip:
+			self.elapsed_time = clip["time_step"]
+			return clip
+		return None
+
+	#predice uno stato futuro e ritnorna il tempo trascorso
+	def predState(self, time_elapsed):
+		return {"time": time_elapsed} #dizionario chiave("time"):valore(time_elapsed)
+	
+	#avanza la simulazione velocemente, di n steps, senza i ritardi di time.sleep
+	def fastForwardNSteps(self, steps=1): 
+		for i in range(steps):
+			self.nextStep() #avanza il tempo di 1 passo
+
+			#predizione o calcolo del nuovo stato -> metodo predState
+			self.current_state = self.predState(self.elapsed_time)  #aggiorna lo stato corrente con lo stato predetto
+			self.state_log.append(self.current_state)
+		if len(self.state_log) > self.MAX_STATE_LOG:
+			self.state_log = self.state_log[-self.MAX_STATE_LOG:]
+
+	#riavvolge la simulazione di un certo numero di passi
+	#servono abbastanza state_log
+	def rewindNSteps(self, steps=1):
+
+		#len(...) = quanti stati salvati
+		#1: controllare se ci sono abbastanza stati salvati
+		if len(self.state_log) >= steps: 
+			#2: aggiornare lo stato corrente con lo stato indietro di n steps
+			state = self.state_log[-steps] 
+			self.elapsed_time = state["time"]
+			self.current_state = state["state"]
+			#con unpacking -> self.elapsed_time, self.current_state = self.state_log[-steps]
+
+			#3: cancellare tutto il log successivo al nuovo stato corrente
+			self.state_log = self.state_log[:len(self.state_log)-steps]
+		else:
+			print(f"Impossibile riavvolgere di {steps} passi. Disponibili {len(self.state_log)} stati")
+
